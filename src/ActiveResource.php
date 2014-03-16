c3449029bc300aa34dca8cbb19b7bcbe14645ece
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2014-03-16 16:31:41 +0200

    going back to instances

--- src/ActiveResource.php
+++ src/ActiveResource.php
@@ -4,6 +4,7 @@ namespace ActiveResource;
 
 require_once 'vendor/autoload.php';
 use Guzzle\Http\Client;
+use Guzzle\Http\Exception;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
@@ -47,7 +48,7 @@ use Monolog\Handler\StreamHandler;
  *
  *
 
- ?>
+   ?>
  *
  * @author John Luxford
  <lux@companymachine.com>
@@ -58,64 +59,72 @@ class ActiveResource {
 	/**
 	 * The REST site address, e.g., http://user:pass@domain:port/
 	 */
-	protected static $site = false;
+	var $site = false;
 
 	/**
 	 * The remote collection, e.g., person or thing
 	 */
-	public static $element_name = false;
+	var $element_name = false;
 
 	/**
 	 * Pleural form of the element name, e.g., people or things
 	 */
-	public  static $element_name_plural = '';
+	var $element_name_plural = '';
 
 	/**
 	 * Corrections to improper pleuralizations.
 	 */
-	protected static $pleural_corrections = array('persons' => 'people', 'peoples' => 'people', 'mans' => 'men', 'mens' => 'men', 'womans' => 'women', 'womens' => 'women', 'childs' => 'children', 'childrens' => 'children', 'sheeps' => 'sheep', 'octopuses' => 'octopi', 'quizs' => 'quizzes', 'axises' => 'axes', 'buffalos' => 'buffaloes', 'tomatos' => 'tomatoes', 'potatos' => 'potatoes', 'oxes' => 'oxen', 'mouses' => 'mice', 'matrixes' => 'matrices', 'vertexes' => 'vertices', 'indexes' => 'indices', );
+	var $pleural_corrections = array('persons' => 'people', 'peoples' => 'people', 'mans' => 'men', 'mens' => 'men', 'womans' => 'women', 'womens' => 'women', 'childs' => 'children', 'childrens' => 'children', 'sheeps' => 'sheep', 'octopuses' => 'octopi', 'quizs' => 'quizzes', 'axises' => 'axes', 'buffalos' => 'buffaloes', 'tomatos' => 'tomatoes', 'potatos' => 'potatoes', 'oxes' => 'oxen', 'mouses' => 'mice', 'matrixes' => 'matrices', 'vertexes' => 'vertices', 'indexes' => 'indices', );
 
 	
 	
-	private $data=false;
-		
-		
-	public function __construct($data=false){
+	var $data=false;
+	var $scope=false;
+    
+    //---- in the init
+	var $client;
+	var $log;
+
+	
+	public function __construct($data=false, $scope=false){
 		if ($data){
 			$this->data = $data;
 		}
+		
+		if($scope)
+		{
+			$this -> scope = $scope;
+			foreach($scope as $k => $v)
+			{
+				$this->site = $this->site.$k.'/'.$v.'/';
+			}
+		}
+        
+        $this->element_name = $this->element_name ? $this->element_name : strtolower (get_class ($this));
+        
+        // Detect for namespaces, and take just the class name
+		if (stripos ($this->element_name, '\\')) {
+			$classItems = explode ('\\', $this->element_name);
+			$this->element_name = end ($classItems);
+		}
+        
+        // Get the plural name after removing namespaces
+		$this->element_name_plural = $this->pluralize ($this->element_name);
+        
+        
+        $this->client = new Client();
+        $this->init_log();
+	
+  
+
 	}	
 		
 	
-	//---- in the init
-	protected static $client;
-	protected static $log;
 	
-
-	public static function init() {
-
-		static::$client = new Client();
-		static::init_log();
-
-		// Allow class-defined element name or use class name if not defined
-		static::$element_name = static::$element_name ? static::$element_name : strtolower(get_called_class());
-		
-		// Detect for namespaces, and take just the class name
-		if (stripos(static::$element_name, '\\')) {
-			$classItems = explode('\\', static::$element_name);
-			static::$element_name = end($classItems);
-		}
-
-		// Get the plural name after removing namespaces
-        //echo "element name is " . static::$element_name;
-		static::$element_name_plural = static::pluralize(static::$element_name);
-        //echo "element name is " . static::$element_name_plural;
-	}
-
 	/**
 	 * Pluralize the element name.
 	 */
-	private static function pluralize($word) {
+    function pluralize($word) {
 		$word .= 's';
 		$word = preg_replace('/(x|ch|sh|ss])s$/', '\1es', $word);
 		$word = preg_replace('/ss$/', 'ses', $word);
@@ -124,8 +133,8 @@ class ActiveResource {
 		$word = preg_replace('/([^aeiouy]|qu)ys$/', '\1ies', $word);
 		$word = preg_replace('/(?:([^f])fe|([lr])f)s$/', '\1\2ves', $word);
 		$word = preg_replace('/ieses$/', 'ies', $word);
-		if (isset(static::$pleural_corrections[$word])) {
-			return static::$pleural_corrections[$word];
+		if (isset($this -> $pleural_corrections[$word])) {
+			return $this -> $pleural_corrections[$word];
 		}
 		return $word;
 	}
@@ -145,12 +154,12 @@ class ActiveResource {
 	 */
 	function save() {
 		//this is an update
-		if (isset($this -> _data['id'])) {
-			return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '/' . $this -> _data['id'] . '.json', 'PUT', $this -> _data);
+		if (isset($this -> data['id'])) {
+			return $this -> _send_and_receive($this -> site . $this -> element_name_plural.'/'.$this -> data['id']. '.json', 'PUT', array($this -> element_name => $this -> data));
 			// update
 		}
 		// this is a create
-		return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '.json', 'POST', $this -> _data);
+		return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '.json', 'POST', array($this -> element_name => $this -> data));
 		// create
 	}
 
@@ -160,9 +169,12 @@ class ActiveResource {
 	 *     POST /collection.json
 
 	 */
-	public static function update($id, $data) {
+	function update($id, $data) {
 		// this is a create
-		return static::_send_and_receive(static::$site . static::$element_name_plural.'/'.$id. '.json', 'PUT', array(static::$element_name => $data));
+		if (isset($this -> data['id'])) {
+			$id = $this -> data['id'];
+		}
+		return $this -> _send_and_receive($this -> site . $this -> element_name_plural.'/'.$id. '.json', 'PUT', array($this -> element_name => $data));
 	}
 
 
@@ -171,9 +183,9 @@ class ActiveResource {
 	 *
 	 *     POST /collection.json
 	 */
-	public static function create($data) {
+	function create($data) {
 		// this is a create
-		return static::_send_and_receive(static::$site . static::$element_name_plural . '.json', 'POST', array(static::$element_name => $data));
+		return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '.json', 'POST', array($this -> element_name => $data));
 	}
 
 
@@ -195,17 +207,17 @@ class ActiveResource {
 	 *     GET /collection/id.json
 	 *     GET /collection.json
 	 */
-	public static function find($id, $options = array ()) {
+	function find($id, $options = array ()) {
 		$options_string = '';
 		if (count($options) > 0) {
 			$options_string = '?' . http_build_query($options);
 		}
 		if ($id == 'all') {
-            //echo ("static site isss ".static::$element_name_plural);
-			$url = static::$site . static::$element_name_plural . '.json';
-			return static::_send_and_receive($url . $options_string, 'GET');
+            
+			$url = $this -> site . $this -> element_name_plural . '.json';
+			return $this -> _send_and_receive($url . $options_string, 'GET');
 		}
-		return static::_send_and_receive(static::$site . static::$element_name_plural . '/' . $id . '.json' . $options_string, 'GET');
+		return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '/' . $id . '.json' . $options_string, 'GET');
 	}
 
 	/**
@@ -213,8 +225,8 @@ class ActiveResource {
 	 *
 	 *     GET /collection.json
 	 */
-	public static function findall($options = array ()) {
-		static::find('all', $options);
+	function findall($options = array ()) {
+		return $this -> find('all', $options);
 	}
 
 	/**
@@ -223,13 +235,13 @@ class ActiveResource {
 	 *     GET /collection/id/action.json
 	 *     GET /collection/id/action.json?attr=value 
 	 */
-	public static function get($id, $method, $options = array ()) {
+	function get($id, $method, $options = array ()) {
 		$options_string = '';
 		if (count($options) > 0) {
 			$options_string = '?' . http_build_query($options);
 		}
 
-		return static::_send_and_receive(static::$site . static::$element_name_plural . '/' . $id .'/'.$method .'.json' . $options_string, 'GET');		
+		return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '/' . $id .'/'.$method .'.json' . $options_string, 'GET');
 	}
 
 	/**
@@ -244,7 +256,7 @@ class ActiveResource {
 		}
 		$req .= '/' . $method . '.json';
 		return $this -> _send_and_receive($req, 'POST', $options, $start_tag);
-		return static::_send_and_receive(static::$site . static::$element_name_plural . '/' . $id .'/'.$method .'.json' . $options_string, 'GET');		
+		return $this -> _send_and_receive($this -> site . $this -> element_name_plural . '/' . $id .'/'.$method .'.json' . $options_string, 'GET');
 		
 	}
 
@@ -271,8 +283,9 @@ class ActiveResource {
 	/**
 	 * Build the request, call _fetch() and parse the results.
 	 */
-	private static function _send_and_receive($url, $method, $data = array ()) {
-
+	function _send_and_receive($url, $method, $data = array ()) {
+		try {
+		
 		//1. Create the qreuest
 		$request = null;
 		$json = "";
@@ -282,72 +295,244 @@ class ActiveResource {
 		switch ($method) {
 			case 'POST' :
 				$json = json_encode($data);
-				$request = static::$client -> post($url, $post_headers, $json);
+				$request = $this -> client -> post($url, $post_headers, $json);
 				break;
 			case 'DELETE' :
 				//curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
 				break;
 			case 'PUT' :
 				$json = json_encode($data);
-				$request = static::$client -> put($url, $post_headers, $json);
+				$request = $this -> client -> put($url, $post_headers, $json);
 				break;
 			case 'GET' :
-				$request = static::$client -> get($url);
+				$request = $this -> client -> get($url);
 			default :
 				break;
 		}
 
-		if (static::$access_token) {
-			$request -> addHeader('AUTHORIZATION', "Token token=" . static::$access_token);
+		if ($this -> access_token) {
+			$request -> addHeader('AUTHORIZATION', "Token token=" . $this -> access_token);
 		}
 		//2. add auth headers
 
-		static::$log -> addDebug($request -> getMethod() . " " . $request -> getURL() . "\n" . $json);
-		//3. send the request
-		$response = $request -> send();
-
-		//4. Decode the response and return
-		static::$log -> addDebug("\n" . $response -> getStatusCode() . " " . $response -> getReasonPhrase() . "\n" . $response -> getBody());
+		$this -> log -> addDebug($request -> getMethod() . " " . $request -> getURL() . "\n" . $json);
 		
-		static::$log -> addDebug("------------------------------\n");		
-		return json_decode($response -> getBody());
+		
+		//3. send the request
+			
+		
+			
+		$response = $request -> send();
+		
+		
+		
+		
+		echo 'hereeeeeeee';
+		//4. Decode the response and return
+		$this -> log -> addDebug("\n" . $response -> getStatusCode() . " " . $response -> getReasonPhrase() . "\n" . $response -> getBody());
+		
+		$this -> log -> addDebug("------------------------------\n");
+		$response_data = json_decode($response -> getBody());
+	
+		if(is_array($response_data))
+		{
+		// if returns an array of objects
+		$res = array ();
+			$cls = get_class ($this);
+			foreach ($response_data as $child) {
+				$obj = new $cls(false, $this->scope);
+				foreach ((array) $child as $k => $v) {
+					//$k = str_replace ('-', '_', $k);
+					if (isset ($v['nil']) && $v['nil'] == 'true') {
+						continue;
+					} else {
+						$obj->data[$k] = $v;
+					}
+				}
+				//print_r($obj -> data);
+				//$this -> log -> addDebug($obj -> data);
+				$res[] = $obj;
+			}
+		//print_r($res);
+		//var_dump($res);
+		return $res;
+		}
+		
+		else{
+			// if returns just one object
+				$cls = get_class ($this);
+				$obj = new $cls(false, $this->scope);
+				foreach ((array) $response_data as $k => $v) {
+					//$k = str_replace ('-', '_', $k);
+					if (isset ($v['nil']) && $v['nil'] == 'true') {
+						continue;
+					} else {
+						$obj->data[$k] = $v;
+					}
+				}
+			$this -> log -> addDebug($obj -> data);
+			return $obj;
+		}
+		
+		}catch (\Exception $e) {
+			$this -> log -> addDebug("\n Exception " . $e->getResponse() -> getBody());	 
+		}
 
 	}
-
-	protected function init_log() {
+	function init_log() {
 		date_default_timezone_set('Europe/Stockholm');
-		static::$log = new Logger('ActiveResource');
+		$this -> log = new Logger('ActiveResource');
 		//$this->log->pushHandler(new StreamHandler('ActiveResource.log', Logger::DEBUG));
 	}
+
+		/**
+	 * Getter for internal object data.
+	 */
+	function __get ($k) {
+		if (isset ($this->data[$k])) {
+			return $this->data[$k];
+		}
+		return $this->{$k};
+	}
+
+	/**
+	 * Setter for internal object data.
+	 */
+	function __set ($k, $v) {
+		if (isset ($this->data[$k])) {
+			$this->data[$k] = $v;
+			return;
+		}
+		$this->{$k} = $v;
+	}
+
+	/**
+	 * Quick setter for chaining methods.
+	 */
+	function set ($k, $v = false) {
+		if (! $v && is_array ($k)) {
+			foreach ($k as $key => $value) {
+				$this->data[$key] = $value;
+			}
+		} else {
+			$this->data[$k] = $v;
+		}
+		return $this;
+	}
+	
+	
+	
 
 }
 
 
 class ApiUser extends ActiveResource{
-	public static $element_name = 'api_user';
-    public static $element_name_plural = 'api_users';
-	public static $site = 'http://localhost:3000/accounts/2/';
-//	public static $access_token = "eba5bef1b170df3f300f724d168ca1a1";
-    public static $access_token = "c5833f3f9668296ee596bfa24c4416f3";
+    var $element_name = 'api_user';
+    var $site = 'http://localhost:3000/';///accounts/2/';
+    //$access_token = "eba5bef1b170df3f300f724d168ca1a1";
+    var $access_token = "c5833f3f9668296ee596bfa24c4416f3";
 }
-ApiUser::init();
+
 
 class Track extends ActiveResource {
-	public static $element_name = 'track';
-    public static $element_name_plural = 'tracks';
-	public static $site = 'http://localhost:3000/accounts/2/';
-//	public static $access_token = "eba5bef1b170df3f300f724d168ca1a1";
-    public static $access_token = "c5833f3f9668296ee596bfa24c4416f3";
+    var $element_name = 'track';
+    var $site = 'http://localhost:3000';
+    //$access_token = "eba5bef1b170df3f300f724d168ca1a1";
+    var $access_token = "c5833f3f9668296ee596bfa24c4416f3";
 }	
 
-Track::init();
 
 
 
 
 
+//// account_admin can access account 2 details
+//
+// $a = new ApiUser(false, array("accounts" => 2));
+// $user_4 = $a->find(4);
+// var_dump($user_4 -> data);
+// 
+// $user_4 -> log -> addDebug($user_4 -> data);
+// $user_4 -> email = 'new_email@skilab.com';
+// $user_4 -> save();
+// 
+// // account_admin can't access account 1 details
+//
+// $a = new ApiUser(false, array("accounts" => 1));
+// $user_4 = $a->find(2);
+// var_dump($user_4 -> data);
+// 
+// // superadmin can't access account 2 details
+// 
+// $b = new ApiUser(false, array("accounts"=> 2));
+// $b -> access_token = "79c173f2eb55ae717138211001ff6b04"; //super admin
+// $user_4 = $b->find(4);
+// 
+// // superadmin can access account 1 details
+// 
+// $b = new ApiUser(false, array("accounts"=> 1));
+// $b -> access_token = "79c173f2eb55ae717138211001ff6b04"; //super admin
+// $user_4 = $b->find(2);
+// 
+// 
+ // Testing api_users //
+ 
+ // admin //
+ 
+ 
+ // can create new account_admin
+ 
+// $b = new ApiUser(false, array("accounts"=> 1));
+// $b -> access_token = "79c173f2eb55ae717138211001ff6b04"; //super admin
+// $user_4 = $b-> create(array("email" => "account_admin_new@skilab.com"));
+//
+//// can't change role
+// $user_4 -> access_token = "79c173f2eb55ae717138211001ff6b04";
+// $user_4 -> role_id = 1;
+// $user_4 -> save();
+ //
+ 
+ // testing validation errors. // gives 422 email cant be blank
+ 
+  $b = new ApiUser(false, array("accounts"=> 1));
+  $b -> access_token = "79c173f2eb55ae717138211001ff6b04"; //super admin
+  $user_4 = $b-> create(array("email" => ""));
 
 
+ 
+ // I think we should have one class for each role (super, account, user) and that's it
+ // The rest is controlled with scope.
+ 
+ // if no scope, then in account's scope (from element name)
+ // so we create a new instance and pass element_name and scope. 
+ 
+ 
+// $t = new Track();
+// $t -> log -> addDebug("----------Element Names-------------\n");
+// $a -> log -> addDebug("ApiUser: " . $a -> element_name . "\n");
+// $t -> log -> addDebug("Track: " . $t -> element_name . "\n");
+// $t -> log -> addDebug("------------------------------\n");
+ 
+ 
+ 
+ // Get all api_users
+// $all = $a -> findall();
+ //echo 'all is' . $all[0] ;
+// print_r($all[1] -> data);
+ 
+  // Get all tracks
+// $t -> findall();
+ 
+  // Get one api_user
+// $b = $a -> find(4);
+ 
+ //echo "B ISSSSSSSSS " . $b -> data['email'];
+ // updating a value
+// $b -> set ('email', 'user-00admin@skilab.com') -> save ();
+ 
+// echo "B ISSSSSSSSS " . $b -> data['email'];
+ 
+// $b->update(null, array('email' => 'adsfadaf@bla.com', 'role'=>2))
 //echo ApiUser::$element_name;
 //echo Track::$element_name;
 //echo ApiUser::$element_name;
@@ -356,8 +541,8 @@ Track::init();
 //echo Track::$element_name_plural;
 //echo ApiUser::$element_name_plural;
 
-echo ApiUser::$element_name_plural;
-ApiUser::findall();
+//echo ApiUser::$element_name_plural;
+//ApiUser::findall();
 //$account_scope = array("accounts"=>2);
 //$track_scope = array("accounts"=>2, 'tracks'=>1);
 //$a = ApiUser::find('4', $account_scope);
